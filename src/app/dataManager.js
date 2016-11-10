e450f5571a9dcd3971bf20902bf80c1835b7e969
Veen <bveenema@gmail.com>
2016-11-10 06:10:43 -0500

    Save Place

--- src/app/dataManager.js
+++ src/app/dataManager.js
@@ -1,9 +1,19 @@
 'use strict'
 
 // const Sensor = require('../models').Sensor
-// const Data = require('../models').Data
+const Data = require('../models').Data
+const mongoose = require('mongoose')
+const moment = require('moment')
 
 const dataManager = {}
+
+dataManager.newSensor = (sID, ownedBy, callback) => {
+  if (sID == null || ownedBy == null) {
+    return callback(new Error('sID or ownedBy missing'))
+  }
+  dataManager.createDataSeries(sID, ownedBy)
+  return callback()
+}
 
 dataManager.recieveData = (data) => {
   const requiredKeys = ['sID', 'data', 'time', 'ownedBy']
@@ -12,6 +22,19 @@ dataManager.recieveData = (data) => {
     return true
   }
   return false
+}
+
+dataManager.createDataSeries = (sID, ownedBy) => {
+  const startData = {
+    series: new mongoose.Types.ObjectId(),
+    ownedBy,
+    prevEnd: moment(0).toDate(),
+    nextStart: moment().year(30000).toDate(),
+  }
+  const d = new Data(startData)
+  d.save((err, doc) => {
+    // left off here
+  })
 }
 
 dataManager.processData = (data) => {
--- test/app/dataManagerTest.js
+++ test/app/dataManagerTest.js
@@ -4,12 +4,29 @@ const sinon = require('sinon')
 const dataManager = require('../../src/app/dataManager')
 
 describe('DataManager', () => {
-  const dataPacket = {
+  const dummySensor = {
     sID: '54321',
-    data: -0.83333,
-    time: new Date(),
     ownedBy: '12345',
   }
+  const dataPacket = Object.assign({},
+    dummySensor,
+    { data: -0.83333, time: new Date() }
+  )
+
+  describe('newSensor(sID, ownedBy, callback)', () => {
+    it('should callback error if sID or ownedBy missing', () => {
+      dataManager.newSensor(null, null, (err) => {
+        err.message.should.be.eql('sID or ownedBy missing')
+      })
+    })
+    it('should call createDataSeries', sinon.test(function () { // eslint-disable-line func-names
+      this.spy(dataManager, 'createDataSeries')
+      dataManager.newSensor(dummySensor.sID, dummySensor.ownedBy, () => {})
+
+      sinon.assert.callCount(dataManager.createDataSeries, 1)
+      sinon.assert.calledWith(dataManager.createDataSeries, dummySensor.sID, dummySensor.ownedBy)
+    }))
+  })
 
   describe('recieveData(data)', () => {
     it('should acknowledge data recieved', () => {
